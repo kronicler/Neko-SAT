96708aee4f9a4e0aa622adf2f4cab85307f78289
lws803 <lws803@gmail.com>
2018-02-13 19:04:55 +0800

    Fixed indentations

--- Debug code/xbee_basestation/xbee_basestation.ino
+++ Debug code/xbee_basestation/xbee_basestation.ino
@@ -1,3 +1,4 @@
+
 #include <XBee.h>
 
 // create the XBee object
@@ -5,8 +6,8 @@ XBee xbee = XBee();
 
 uint8_t payload[30] = "\0";
 
-// SH + SL Address of receiving XBee                             // Remote XBee    
- XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x4098DA08); // address of the receiver XBee
+// SH + SL Address of receiving XBee                             // Remote XBee
+XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x4098DA08); // address of the receiver XBee
 // Or 0x4098DA02
 
 ZBRxResponse        rx  = ZBRxResponse();
@@ -20,81 +21,83 @@ int statusLed = 10; // green
 int errorLed  = 11; // red
 int dataLed   = 12; // yellow
 
-void flashLed(int pin, int times, int wait) 
+void flashLed(int pin, int times, int wait)
 {
-  for (int i = 0; i < times; i++) 
-  {
-    digitalWrite(pin, HIGH);
-    delay(wait);
-    digitalWrite(pin, LOW);
-    if (i + 1 < times) delay(wait);
-  }
+    for (int i = 0; i < times; i++)
+    {
+        digitalWrite(pin, HIGH);
+        delay(wait);
+        digitalWrite(pin, LOW);
+        if (i + 1 < times) delay(wait);
+    }
 }
 
 void setup()
 {
-  pinMode(statusLed, OUTPUT);
-  pinMode(errorLed,  OUTPUT);
-  pinMode(dataLed,   OUTPUT);
- 
-  // start xbee in serial 2
-  Serial2.begin(9600);
-  Serial.begin (9600);
-  xbee.setSerial(Serial2);
-// xbee.begin(9600);
+    pinMode(statusLed, OUTPUT);
+    pinMode(errorLed,  OUTPUT);
+    pinMode(dataLed,   OUTPUT);
+    
+    // start xbee in serial 2
+    Serial2.begin(9600);
+    Serial.begin (9600);
+    xbee.setSerial(Serial2);
+    // xbee.begin(9600);
 }
 
 void loop()
-{  
-   // Add a detector to see if arduino has reecived any serial inputs so that it wont spam
-   // continuously 
-   String myString = "";
-   myString = Serial.readString();
-   if (myString.length() > 0) {
-    for (int i = 0; i < 30; i++)
-      payload[i] = "\0";
-
+{
+    // Add a detector to see if arduino has reecived any serial inputs so that it wont spam
+    // continuously
+    String myString = "";
+    myString = Serial.readString();
+    /**
+    if (myString.length() > 0) {
+        for (int i = 0; i < 30; i++)
+            payload[i] = "\0";
+    */
     Serial.println ("Message sent!");
     myString[myString.length()] = "\n";
     myString.toCharArray(payload, 29);
-   }
-   
-   xbee.send(zbTx);
-   // flash TX indicator
-   flashLed(dataLed, 1, 10);
     
-   // after sending a tx request, we expect a status response
-   // wait up to half a second for the status response
-   if (xbee.readPacket(500))
-   {
-      xbee.getResponse().getZBRxResponse(rx);
-
-      String *inData;
-      uint8_t* rxData = rx.getData();
-      Serial.print (String((char *)rxData)); // The data already includes a new line
-     // got a response!
-     // should be a znet tx status              
-     if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE)
-     {
-        xbee.getResponse().getZBTxStatusResponse(txStatus);
-        // get the delivery status, the fifth byte
-        if (txStatus.getDeliveryStatus() == SUCCESS) 
+    xbee.send(zbTx);
+    // flash TX indicator
+    flashLed(dataLed, 1, 10);
+    
+    
+    // after sending a tx request, we expect a status response
+    // wait up to half a second for the status response
+    if (xbee.readPacket(500))
+    {
+        xbee.getResponse().getZBRxResponse(rx);
+        
+        String *inData;
+        uint8_t* rxData = rx.getData();
+        Serial.print (String((char *)rxData)); // The data already includes a new line
+        // got a response!
+        // should be a znet tx status
+        if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE)
         {
-          // success.  time to celebrate
-          // flashLed(statusLed, 5, 10); 
-        flashLed(statusLed, 1, 10); 
-        } 
-        else
-        {
-          // the remote XBee did not receive our packet. is it powered on?
-          flashLed(errorLed, 1, 50);
+            xbee.getResponse().getZBTxStatusResponse(txStatus);
+            // get the delivery status, the fifth byte
+            if (txStatus.getDeliveryStatus() == SUCCESS)
+            {
+                // success.  time to celebrate
+                // flashLed(statusLed, 5, 10);
+                flashLed(statusLed, 1, 10);
+            }
+            else
+            {
+                // the remote XBee did not receive our packet. is it powered on?
+                flashLed(errorLed, 1, 50);
+            }
         }
-      }      
-   } 
-   else 
-   {
-     // local XBee did not provide a timely TX Status Response -- should not happen
-     flashLed(errorLed, 5, 50);
-   }
-   delay(1000);
+    }
+    else
+    {
+        // local XBee did not provide a timely TX Status Response -- should not happen
+        flashLed(errorLed, 5, 50);
+    }
+    delay(1000);
 }
+
