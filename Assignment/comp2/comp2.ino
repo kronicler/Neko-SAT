10275cc39d4195973d9f684ed9a13ffd94320201
lws803 <lws803@gmail.com>
2018-02-20 00:36:44 +0800

    wow

--- Assignment/comp2/comp2.ino
+++ Assignment/comp2/comp2.ino
@@ -1,4 +1,3 @@
-
 #include <XBee.h>
 #include <Wire.h> //I2C Arduino Library
 
@@ -7,13 +6,13 @@
 
 XBee xbee = XBee();
 
-// create reusable response objects for responses we expect to handle 
+// create reusable response objects for responses we expect to handle
 XBeeResponse response   = XBeeResponse();
 ZBRxResponse        rx  = ZBRxResponse();
 ModemStatusResponse msr = ModemStatusResponse();
 
 
-// Sending end 
+// Sending end
 
 uint8_t payload[30] = "\0";
 XBeeAddress64 addr64 = XBeeAddress64(0x0013A200, 0x4098DA02);
@@ -40,118 +39,114 @@ void getHMC (int *x, int *y, int *z) {
     }
 }
 
+float getHumidity () {
+    return (float)analogRead(A0)*100 /1024;
+}
 
-void setup() 
+void setup()
 {
-  /**
-  pinMode(statusLed, OUTPUT);
-  pinMode(errorLed,  OUTPUT);
-  pinMode(dataLed,   OUTPUT);
-  pinMode(led,       OUTPUT);
-  */
-  // start xbee in serial 2
-  Serial2.begin(9600);
-  xbee.setSerial(Serial2);
-  Serial.begin(9600);
-
-  Wire.begin();
-  
-  //Put the HMC5883 IC into the correct operating mode
-  Wire.beginTransmission(HMC_address); //open communication with HMC5883
-  Wire.write(0x02); //select mode register
-  Wire.write(0x00); //continuous measurement mode
-  Wire.endTransmission();
+    
+    Serial2.begin(9600);
+    xbee.setSerial(Serial2);
+    Serial.begin(9600);
+    
+    Wire.begin();
+    
+    //Put the HMC5883 IC into the correct operating mode
+    Wire.beginTransmission(HMC_address); //open communication with HMC5883
+    Wire.write(0x02); //select mode register
+    Wire.write(0x00); //continuous measurement mode
+    Wire.endTransmission();
 }
 
 // continuously reads packets, looking for ZB Receive or Modem Status
-void loop() 
+void loop()
 {
     xbee.readPacket();
     if (xbee.getResponse().isAvailable())
     {
-      // Serial.println("Received");
-
-      // got something
-      if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) 
-      {
-        // got a zb rx packet
-        // now fill our zb rx class
-        xbee.getResponse().getZBRxResponse(rx);
-     //   if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED) 
-        if (rx.getOption() == 65) 
-        {
-           // the sender got an ACK
-        } 
-        else 
-        {                     
-        }
-
+        // Serial.println("Received");
         
-        // get data
-        String *inData;
-        uint8_t* rxData = rx.getData();
-
-        Serial.print (String((char *)rxData)); // The data already includes a new line
-        String receiver = String((char *)rxData); 
-        if (receiver == "temp\n") {
-          // Send back temperature data here. 
-          String myString = "hello chin chin";
-          //myString = Serial.readString();
-          if (myString.length() > 0) {
-            for (int i = 0; i < 30; i++)
-            payload[i] = "\0";
-
-            Serial.println ("Message replied!");
-            myString[myString.length()] = "\n";
-            myString.toCharArray(payload, 29);
-          }
-   
-          xbee.send(zbTx);
-        }
-        else if (receiver == "hmc\n") {
-          // Fill in humidity response here. 
-          // Send back temperature data here.
-          int x, y, z;
-          getHMC (&x, &y, &z);
-          String myString = String(x);
-          myString += " ";
-          myString += String (y);
-          myString += " ";
-          myString += String (z);
-
-          if (myString.length() > 0) {
-            for (int i = 0; i < 30; i++)
-            payload[i] = "\0";
-
-            Serial.println ("Message replied!_humidity");
-            myString[myString.length()] = "\n";
-            myString.toCharArray(payload, 29);
-          }
-   
-          xbee.send(zbTx);
-        }
-      } 
-      else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE)
-      {
-        xbee.getResponse().getModemStatusResponse(msr);
-        // the local XBee sends this response on certain events, 
-        // like association/dissociation
-        if (msr.getStatus() == ASSOCIATED) 
+        // got something
+        if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE)
         {
-          // yay this is great.  flash led
-        } 
-        else if (msr.getStatus() == DISASSOCIATED) 
-        {
-          // this is awful.. flash led to show our discontent
+            // got a zb rx packet
+            // now fill our zb rx class
+            xbee.getResponse().getZBRxResponse(rx);
+            //   if (rx.getOption() == ZB_PACKET_ACKNOWLEDGED)
+            if (rx.getOption() == 65)
+            {
+                // the sender got an ACK
+            }
+            else
+            {
+            }
+            
+            
+            // get data
+            String *inData;
+            uint8_t* rxData = rx.getData();
+            String receiver = String((char *)rxData);
+
+            Serial.print (receiver); // The data already includes a new line
+            if (receiver == "humidity\n") {
+                String myString = String(getHumidity());
+                //myString = Serial.readString();
+                if (myString.length() > 0) {
+                    for (int i = 0; i < 30; i++)
+                        payload[i] = "\0";
+                    
+                    Serial.println ("Message replied!_humidty");
+                    myString[myString.length()] = "\n";
+                    myString.toCharArray(payload, 29);
+                }
+                
+                xbee.send(zbTx);
+            }
+            else if (receiver == "hmc\n") {
+                // Fill in humidity response here.
+                // Send back temperature data here.
+                int x, y, z;
+                getHMC (&x, &y, &z);
+                String myString = String(x);
+                myString += " ";
+                myString += String (y);
+                myString += " ";
+                myString += String (z);
+                
+                if (myString.length() > 0) {
+                    for (int i = 0; i < 30; i++)
+                        payload[i] = "\0";
+                    
+                    Serial.println ("Message replied!_hmc");
+                    myString[myString.length()] = "\n";
+                    myString.toCharArray(payload, 29);
+                }
+                
+                xbee.send(zbTx);
+            }
         }
-        else 
+        else if (xbee.getResponse().getApiId() == MODEM_STATUS_RESPONSE)
         {
-          // another status
+            xbee.getResponse().getModemStatusResponse(msr);
+            // the local XBee sends this response on certain events,
+            // like association/dissociation
+            if (msr.getStatus() == ASSOCIATED)
+            {
+            }
+            else if (msr.getStatus() == DISASSOCIATED)
+            {
+                // this is awful.. flash led to show our discontent
+            }
+            else
+            {
+                // another status
+            }
         }
-      }
-      else
-      {
-        // not something we were expecting
-      }
+        else
+        {
+            // not something we were expecting
+        }
     }
 }
+
